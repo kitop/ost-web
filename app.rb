1fd789891f4242222d10c15f57d4f26cfdd05c49
Esteban Pastorino <ejpastorino@gmail.com>
2015-06-09 10:53:12 -0400

    re enqueue and remove links

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ Cuba.plugin Cuba::Render
 Cuba.settings[:render][:layout] = "layout.html"
 
 Cuba.use Rack::Static, urls: ["/css"], root: "public"
+Cuba.use Rack::MethodOverride
 
 Cuba.define do
   on get, root do
@@ -26,5 +27,21 @@ Cuba.define do
 
   on get, "backups/:id" do |id|
     render "backup.html", name: id, queue: Ost::Web[id]
+  end
+
+  on post, "backups/:id", param("item") do |id, item|
+    backup = Ost::Web[id]
+    queue = Ost[backup.name]
+
+    queue << item
+
+    res.redirect "/"
+  end
+
+  on delete, "backups/:id", param("item") do |id, item|
+    backup = Ost::Web[id]
+    Ost.redis.call("LREM", backup.key, 1, item)
+
+    res.redirect "/"
   end
 end
